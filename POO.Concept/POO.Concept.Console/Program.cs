32918d3a9afffc7cc17963dc6e6db8ab9fcb61a6
Dark <cristi4n2010@gmail.com>
2025-09-03 20:44:13 -0500

    Se inicia con al creacion del trabajo con fechas

---
+++ POO.Concept/POO.Concept.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿using POO.Concept.Logic;
+
+var date1 = new Date();
+var date2 = new Date(15, 8, 2024);
+
+Console.WriteLine(date1.ToString());
+Console.WriteLine(date2);
---
+++ POO.Concept/POO.Concept.Logic/Date.cs
@@ -0,0 +1,63 @@
+﻿namespace POO.Concept.Logic;
+
+public class Date
+{
+    private int _day;
+    private int _month;
+    private int _year;
+
+    public Date()
+    {
+        Year = 1900;
+        Month = 1;
+        Day = 1;
+    }
+
+    public Date(int day, int month, int year)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+
+    public int Day {
+        get
+        {
+            return _day;
+        }
+        set
+        {
+            _day = value;
+        }
+    }
+    public int Month {
+        get
+        {
+            return _month;
+        }
+        set
+        {
+            _month = value;
+        }
+    }
+    public int Year {
+        get
+        {
+            return _year;
+        }
+        set
+        {
+           _year = value;
+        } 
+    }
+
+    public override string ToString()
+    {
+        return $"{Year:0000}/{Month:00}/{Day:00}";
+    }
+
+    private bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
